a97f9fb52fd1518574e2c1966eeca0c2fe5d6736
higornunes <higor.fernando.nunes@gmail.com>
2017-03-28 00:47:26 -0300

    agora vai

--- index.php
+++ index.php
@@ -28,13 +28,13 @@
 				</article>
 				<article class="item">
 					<h1 class="item_title">
-						<a href="#SMO" rel="nofollow" title="Utilize as redes sociais para atrair pessoas para o seu site/blog" 
+						<a href="#SMO" rel="nofollow" title="Utilize as redes sociais para atrair pessoas para o seu site/blog"
 										alt="[Utilize as redes sociais para atrair pessoas para o seu site/blog]">SMO</a>
 					</h1>
 				</article>
 				<article class="item">
 					<h1 class="item_title">
-						<a href="#email_marketing" rel="nofollow" title="Utilize estratégias para captar emails de possíveis clientes" 
+						<a href="#email_marketing" rel="nofollow" title="Utilize estratégias para captar emails de possíveis clientes"
 										alt="[Utilize estratégias para captar emails de possíveis clientes]">Email Marketing</a>
 					</h1>
 				</article>
@@ -66,26 +66,32 @@
 
 		<article class="banner">
 			<h1>Aqva</h1>
+			<hr>
 			<p class="tagline text-center">Criando estratégias para conectar a sua empresa ao público alvo.</p>
- 
+			<hr>
+
+			<form  class="tagline text-center">
+				<button type="button" class="btn btn-info" ><h3>Quero uma análise grátis!</h3></button>
+			</form>
+
 			<a href="#SEO"><i class="fa fa-angle-double-down text-center"></i></a>
 		</article>
 	</div>
 
-	
+
 
 	<!-- SEO -->
 	<article id="SEO" class="container text-center marginbottom3">
 		<div class="row">
 			<div class="content_text col-xs-12 col-sm-6">
-				<h1>Otimização para mecânismos de busca </h1>
-				<span>Search Engine Optimization</span>
+				<h1>Search Engine Optimization </h1>
+				<h4>Otimização para mecânismos de busca	</h4>
 				<p class="tagline"><strong>Fazer o Google entender o que o seu site contém</strong>  e sobre o que ele está falando é essencial para qualquer site, nós analizamos o código do seu site e otimizamos suas regras.</p>
 				<p class="tagline"><strong>Cada degrau em que o seu site sobe nas buscas do Google</strong>, pode significar um aumento considerável de visitas no seu site.</p>
-			</div>	
+			</div>
 			<div class="content_image col-xs-12  col-sm-6">
-				<img src="./img/SEO.png" alt="[]" title="" class="img-responsive">	
-			</div>		
+				<img src="./img/SEO.png" alt="[]" title="" class="img-responsive">
+			</div>
 		</div>
 	</article>
 
@@ -96,22 +102,22 @@
 				<img src="./img/SEM.png" alt="[]" title="" class="img-responsive">
 			</div>
 			<div class="content_text margintop2 col-xs-12 col-sm-6">
-				<h1 class="marginbottom3">Marketing de mecânismos de busca</h1>
-				<span>Search Engine Marketing</span>
-				<p class="tagline marginbottom1">Antes de iniciar o projeto <strong>fazemos uma pesquisa para descobrir quais palavras-chave seus concorrentes estão usando</strong>, quais estão sendo pouco usadas e qual o seu avatar(público-alvo)</p>
-				<p class="tagline margintop1">Tudo isso para criar uma estratégia que possibilite a sua empresa realmente <strong>converter os visitantes do seu site em clientes</strong></p>
-			</div>		
+				<h1>Search Engine Marketing</h1>
+				<h4>Marketing de mecânismos de busca</h4>
+				<p class="tagline marginbottom1">Antes de iniciar o projeto <strong>é feita uma pesquisa do seu público, bem como, as palavra-chaves mais adequadas para o seu campo de atuação.</strong></p>
+				<p class="tagline margintop1">Tudo isso para criar uma estratégia que possibilite a sua empresa realmente <strong>converter os visitantes do site em clientes.</strong></p>
+			</div>
 		</div>
 	</article>
 	<article id="SMO" class="container text-center">
-		<h1 >Otimização de redes sociais</h1>
-		<span >Social Media Optimization</span>
+		<h1>Social Media Optimization</h1>
+		<h4>Otimização de redes sociais</h4>
 		<div class="content_image">
 			<img src="./img/redes-sociais.png" alt="[]" title="">
 		</div>
-		<p class="tagline ">Estar presente nas redes sociais é importante e pode <strong>trazer mais visitantes ao seu site</strong></p>
-		<p class="tagline ">Utilizamos ferramentas que permitem saber <strong>quantas pessoas estão saindo da sua página no facebook e entrando no seu site</strong>, melhorando nossa estratégia e entendendo cada vez mais o seu público</p>
-		
+		<p class="tagline ">Estar presente nas redes sociais também é importante e pode <strong>trazer mais visitantes.</strong></p>
+		<p class="tagline ">Utilizamos ferramentas que permitem monitorar <strong>o tráfego de visitantes da sua página no facebook e do site</strong>, melhorando sua estratégia constantemente e entendendo mais o seu público alvo.</p>
+
 	</article>
 	<section id="email_marketing" class="text-center">
 		<div class="container">
@@ -119,22 +125,22 @@
 				<h1>3 razões para começar a fazer email marketing</h1>
 				<article class="col-xs-12 col-sm-6">
 					<div class="content_image">
-						<h1><img class="icon_email" src="./img/email-marketing.png" alt="[]" title=""></h1>				
+						<h1><img class="icon_email" src="./img/email-marketing.png" alt="[]" title=""></h1>
 					</div>
-					<p class="tagline">Ao contrário do que muitos pensam o <strong>email não está morrendo</strong>, pelo contrário cada vez mais aquelas correntes, brincadeiras e piadas que recebiamos frequentemente estão saindo do email e indo para o whatsapp. </p>
+					<p class="tagline">Ao contrário do que muitos pensam o <strong>email não está caindo em desuso</strong>, pelo contrário cada vez mais está ferramenta está sendo usada como meio de comunicação eficiente com os clientes.</p>
 				</article>
 				<article class="col-xs-12 col-sm-6">
 					<div class="content_image">
-						<h1><img class="icon_email"  src="./img/email-marketing2.png" alt="[]" title=""></h1>				
+						<h1><img class="icon_email"  src="./img/email-marketing2.png" alt="[]" title=""></h1>
 					</div>
-					<p class="tagline"><strong>Os spams estão ficando cada vez menos recorrentes</strong> devido a facilidade que os usuários tem de denunciar e bloquear aqueles que enviam conteúdo desnecessário. </p>				
+					<p class="tagline"><strong>Os spams estão ficando cada vez menos recorrentes</strong> devido a facilidade que os usuários tem de denunciar e bloquear aqueles que enviam conteúdo desnecessário. </p>
 				</article>
 				<article class="col-xs-12 col-sm-12">
 					<div class="content_image">
-						<h1><img class="icon_email"  src="./img/email-marketing3.png" alt="[]" title=""></h1>				
+						<h1><img class="icon_email"  src="./img/email-marketing3.png" alt="[]" title=""></h1>
 					</div>
 					<p class="tagline"><strong>O email evoluiu para uma ferramenta de trabalho séria</strong> onde as pessoas só querem receber conteúdo de qualidade, nós acreditamos que cada empresa tem muito a oferecer aos seus clientes e trabalhamos para conectar aqueles que tem algo a oferecer com aqueles que precisam!</p>
-				</article>		
+				</article>
 			</div>
 		</div>
 	</section>
@@ -146,14 +152,14 @@
 					<img src="./img/landing-page.png" alt="[]" title="">
 				</div>
 				<div class="content_text col-xs-12 col-md-6">
-					<p class="tagline content"><strong>Landing Pages são páginas que tem obejtivos claros</strong> como: coletar um email, descobrir o interesse do visitante ou vender um produto,</p>
+					<p class="tagline content"><strong>Landing Pages são páginas que tem objetivos claros</strong> como: coletar um email, descobrir o interesse do visitante ou vender um produto,</p>
 				</div>
 				<div class="row">
 					<div class="col-md-6">
-						<p class="tagline">Caso você já tenha um site com outra estrutura e não queira alterá-lo, podemos <strong>criar uma landing page</strong> que é totalmente independente do resto do site.</p>
+						<p class="tagline">Caso você já tenha um site e não quer alterá-lo<strong> criar uma landing page pode ser um boa estratégia para captar novos clientes</strong>.</p>
 					</div>
 					<div class="col-md-6">
-						<p class="tagline">Quando a <strong>landing page estiver começar a gerar resultados</strong> e você achar conveniente podemos aplicar a otimização no resto do site</p>
+						<p class="tagline">Quando a <strong>landing page começar a gerar resultados</strong> e você achar conveniente podemos aplicar a otimização no resto do site.</p>
 					</div>
 				</div>
 			</div>
@@ -162,14 +168,15 @@
 	<article id="visibilidade" class="container">
 		<div class="text-center">
 			<h1 class="marginbottom4">Publicidade na internet</h1>
-			<p class="tagline content_text">Panfletos, outdoors, rádios e outras mídias físicas são formas de anunciar seu produto, porém podemos garantir que esses meios não são tão efetivos quanto a internet</p><p class="tagline"> sabe quando você busca por um produto em um site de vendas e depois entra em outro site e começa a ver propagandas relacionadas ao produto que você estava procurando?</p>
+			<p class="tagline content_text">Panfletos, outdoors, rádios e outras mídias físicas são formas de anunciar seu produto, porém podemos garantir que não é tão efetivos quanto a internet.</p><p class="tagline"> Sabe quando você busca por um produto em um site de vendas e depois entra em outro site e começa a ver propagandas relacionadas ao produto que você estava procurando?</p>
 			<p class="tagline font-bold">É desse tipo de estratégia que estamos falando!!!</p>
 		</div>
 	</article>
 	<article id="blog" class="container">
 		<div class="row margintop4 marginbottom4">
 			<h1>Vale a pena ter um blog?</h1>
-			<p class="tagline">Em que qualquer ramo de negócio e independentemente do tamanho, um blog pode fornecer uma estratégia de marketing digital muito importante que ajudará a consolidar seu produto e sua marca e consequentemente ampliará o seu alcance no mercado. Você não deve ter um blog somente porque seu concorrente tem, nem depositar sua criatividade apenas em redes sociais.</p>
+			<p class="tagline">Em que qualquer ramo de negócio e independentemente do tamanho, um blog pode fornecer uma estratégia de marketing muito valiosa que ajudará a consolidar seu produto e sua marca na internet e consequentemente ampliará o seu alcance no mercado.</p>
+			<p class="tagline"> Você não deve ter um blog somente porque seu concorrente tem, mais sim porque um blog é um estratégia barata que atrair cliente pelo valor do conteúdo que é transmitido por ele.</p>
 		</div>
 	</article>
 	<article id="analise">
@@ -185,7 +192,7 @@
 				    <label for="email">Seu email</label>
 				    <input type="email" class=" campo" id="email">
 				</div> -->
-				<button type="button" class="btn btn-info" >Quero uma análise!</button>
+				<button type="button" class="btn btn-info" >Quero uma análise grátis!</button>
 			</form>
 		</div>
 	</article>
@@ -201,27 +208,27 @@
 				<button type="button" class="btn btn-success">Me envie a cartilha!</button>
 			</form>
 		</div>
-	</article> 
+	</article>
 	<nav>
 		<h1 class="font0"> Teste Aqva - Agência Web</h1>
 	<ul class="row">
 			<li class="col-xs-12 col-sm-2">
-				<a href="#SEO" title="Otimize seu site para o Google e melhore sua posição nas buscas" 
+				<a href="#SEO" title="Otimize seu site para o Google e melhore sua posição nas buscas"
 				alt="Otimize seu site para o Google e melhore sua posição nas buscas">SEO</a>
 			</li>
 			<li class="col-xs-12 col-sm-2">
 				<a href="#SEM" title="Conheça quais palavras chaves são mais utilizadas e melhore seu posicionamento" alt="[Conheça quais palavras chaves são mais utilizadas e melhore seu posicionamento]">SEM</a>
 			</li>
 			<li class="col-xs-12 col-sm-2">
-				<a href="#SMO" title="Utilize as redes sociais para atrair pessoas para o seu site/blog" 
+				<a href="#SMO" title="Utilize as redes sociais para atrair pessoas para o seu site/blog"
 				alt="[Utilize as redes sociais para atrair pessoas para o seu site/blog]">SMO</a>
-			</li>		
+			</li>
 			<li class="col-xs-12 col-sm-2">
-				<a href="#email_marketing" title="Utilize estratégias para captar emails de possíveis clientes" 
+				<a href="#email_marketing" title="Utilize estratégias para captar emails de possíveis clientes"
 				alt="[Utilize estratégias para captar emails de possíveis clientes]">Email Marketing</a>
 			</li>
 			<li class="col-xs-12 col-sm-2">
-				<a href="#landingpage" title="Crie uma landing page para explicar seu produto e fazer sua oferta" alt="[Crie uma landing page para explicar seu produto e fazer sua oferta]">Landing Page</a>		
+				<a href="#landingpage" title="Crie uma landing page para explicar seu produto e fazer sua oferta" alt="[Crie uma landing page para explicar seu produto e fazer sua oferta]">Landing Page</a>
 			</li>
 			<li class="col-xs-12 col-sm-2">
 				<a href="#blog" title="Utilize blogs para melhorar sua presença na internet" alt="[Utilize blogs para melhorar sua presença na internet]">Blog</a>
